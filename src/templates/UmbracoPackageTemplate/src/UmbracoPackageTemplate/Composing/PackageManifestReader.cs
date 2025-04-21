ba1e29a7fed10082972d95521b09055930aeda73
jcdcdev <james@jcdc.dev>
2025-04-21 22:40:25 +0100

    chore: update NuGet packages

--- src/templates/UmbracoPackageTemplate/src/UmbracoPackageTemplate/Composing/PackageManifestReader.cs
+++ src/templates/UmbracoPackageTemplate/src/UmbracoPackageTemplate/Composing/PackageManifestReader.cs
@@ -7,7 +7,7 @@ namespace UmbracoPackageTemplate.Composing;
 
 internal class PackageManifestReader : IPackageManifestReader
 {
-    public async Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
+    public Task<IEnumerable<PackageManifest>> ReadPackageManifestsAsync()
     {
         var extensions = new List<IManifest>();
         var packageManifest = new PackageManifest
@@ -27,6 +27,6 @@ internal class PackageManifestReader : IPackageManifestReader
         });
 
         packageManifest.Extensions = extensions.OfType<object>().ToArray();
-        return [packageManifest];
+        return Task.FromResult<IEnumerable<PackageManifest>>([packageManifest]);
     }
 }
